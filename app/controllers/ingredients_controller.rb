501469bb3e5c88c1548489f728a199fff66edb58
dotnotation <dotinmotionproductions@gmail.com>
2021-09-13 20:35:06 -0400

    set up controllers and basic validations

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,37 @@
 class IngredientsController < ApplicationController
+    before_action :set_ingredient, only: [:show, :edit]
+    def index
+        @ingredients = Ingredient.search(params[:query])
+        render 'index'
+    end
+
+    def show
+    end
+
+    def new
+        @ingredient = Ingredient.new 
+    end
+
+    def create
+        @ingredient = Ingredient.new(ingredient_params)
+
+        if @ingredient.save
+            redirect_to @ingredient
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    private
+
+    def set_ingredient
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,37 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit]
+    def index
+        @recipe = Recipe.search(params[:query])
+        render 'index'
+    end
+
+    def show
+    end
+
+    def new
+        @recipe = Recipe.new 
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save
+            redirect_to @recipe
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:name)
+    end
 end
